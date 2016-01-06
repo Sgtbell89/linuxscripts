8e54f44b82a6539c23bc3c38e56978239cd8e9d2
Matthew <matthewalanpenning@gmail.com>
2016-01-06 12:14:28 -0500

    today

---
+++ homebackup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This script is used to backup the user's home directory
+
+cd /home
+
+tar -cf /var/tmp/homebackup.tar $USER 
+
+rm /var/tmp/homebackup.tar.gz > /dev/null
+
+gzip /var/tmp/homebackup.tar 
+
+scp /var/tmp/homebackup.tar.gz administrator@10.1.10.80:~
+
+echo "Job completed"
+echo
+
